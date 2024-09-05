2877c92561a03c141fd564f905f904898ab40381
Purity Wambua <puritywamb@gmail.com>
2024-09-05 10:20:34 +0300

    fileio

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    file_txt=f"{file_name}.txt"
+    with open(file_txt,mode='w',encoding="utf-8") as new_file:
+       new_file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    file_txt=f"{file_name}.txt"
+    with open(file_txt,mode='a',encoding="utf-8") as new_file:
+       new_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_txt=f"{file_name}.txt"
+    with open(file_txt,encoding="utf-8") as file_name:
+       text=file_name.read()
+       return text
